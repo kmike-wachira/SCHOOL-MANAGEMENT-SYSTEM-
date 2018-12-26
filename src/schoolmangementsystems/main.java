27832dc83feaaaa4e3298e425bbc2cc8c51aab09
kmike-wachira <mwkariuki2e@gmail.com>
2018-12-26 20:12:04 +0300

    initial commit

---
+++ src/schoolmangementsystems/main.java
@@ -0,0 +1,14 @@
+
+package schoolmangementsystems;
+
+
+public class main {
+
+   
+    public static void main(String[] args) {
+        HomePage homepage=new HomePage();
+        homepage.setVisible(true);
+    
+    }
+    
+}
